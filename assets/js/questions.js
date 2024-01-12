bc1eff217cbb47ef70607af3a984225141f33432
thelukass88 <l.holliday88@hotmail.com>
2024-01-12 19:36:30 +0000

    Reorganised JS files and added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+export function questions() {
+    let questionsAndAnswers = [
+        {
+            title: 'An array can be used to collect data on:',
+            answers: [
+                'Only strings',
+                'Only Booleans',
+                'Only Integers',
+                'All data types',
+            ],
+            correct: 3,
+        },
+        {
+            title: 'DOM stands for:',
+            answers: [
+                'Document, Object, Model',
+                'Directory, Object, Mode',
+                'Document, Object, Mirror',
+                'Do not, Offer, Milk',
+            ],
+            correct: 0,
+        },
+        {
+            title: 'var, let and const all operate in the same way and have no distinctions:',
+            answers: [
+                'True',
+                'False',
+            ],
+            correct: 1,
+        },
+        {
+            title: 'Which event would work best with a submit button?',
+            answers: [
+                'keydown or onkeydown',
+                'change or onchange',
+                'click or onclick',
+                'load or onload',
+            ],
+            correct: 2,
+        }
+    ]
+}

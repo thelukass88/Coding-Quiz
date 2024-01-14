f1a0344ddbb99c8a63b3c3ab90078efc6b93fbf9
thelukass88 <l.holliday88@hotmail.com>
2024-01-14 21:19:33 +0000

    Adding const in js file

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+
+const startButton = document.getElementById("startButton");
+const startPage = document.querySelector(".start");
+const questionBox = document.getElementById("questions");
+
+
+//when start button is clicked
+if startButton button clicked
+startButton.onclick = ()=>{
+    startPage.classList.remove("activeInfo"); //hide info box
+   questionBox.classList.add("activeQuiz"); //show quiz box
+
+}
+
+//quiz timer
+let timer

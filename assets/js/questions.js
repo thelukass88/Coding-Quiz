6d0eea60d156104d1a9fe44fb86ac42259b70942
thelukass88 <l.holliday88@hotmail.com>
2024-01-14 20:53:26 +0000

    changed the questions out of a function to a variable

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,42 +1,45 @@
-export function questions() {
-    let questionsAndAnswers = [
-        {
-            title: 'An array can be used to collect data on:',
-            answers: [
-                'Only strings',
-                'Only Booleans',
-                'Only Integers',
-                'All data types',
-            ],
-            correct: 3,
-        },
-        {
-            title: 'DOM stands for:',
-            answers: [
-                'Document, Object, Model',
-                'Directory, Object, Mode',
-                'Document, Object, Mirror',
-                'Do not, Offer, Milk',
-            ],
-            correct: 0,
-        },
-        {
-            title: 'var, let and const all operate in the same way and have no distinctions:',
-            answers: [
-                'True',
-                'False',
-            ],
-            correct: 1,
-        },
-        {
-            title: 'Which event would work best with a submit button?',
-            answers: [
-                'keydown or onkeydown',
-                'change or onchange',
-                'click or onclick',
-                'load or onload',
-            ],
-            correct: 2,
-        }
-    ]
-}
+
+let questionsAndAnswers = [
+         {
+            number: 1, 
+            question: 'An array can be used to collect data on:',
+            answer: "All data types",
+            options: [
+                 'Only strings',
+                 'Only Booleans',
+                 'Only Integers',
+                 'All data types',
+             ]
+         },
+         {
+            number: 2, 
+            question: 'What does DOM stand for:',
+            answer: "Document, Object, Model",
+            options: [
+                 'Document, Object, Model',
+                 'Directory, Object, Mode',
+                 'Document, Object, Mirror',
+                 'Do not, Offer, Milk',
+             ]
+         },
+         {
+            number: 3, 
+            question: 'var, let and const all operate in the same way and have no distinctions:',
+            answer: "False", 
+            options: [
+                 'True',
+                 'False',
+             ]
+         },
+         {
+            number: 4, 
+            question: 'Which event would work best with a submit button?',
+            answer: "click or onclick", 
+            optiond: [
+                 'keydown or onkeydown',
+                 'change or onchange',
+                 'click or onclick',
+                 'load or onload',
+             ]
+         },
+     ];

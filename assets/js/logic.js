ffc268d0dccb245574e27d3b1abba6cb3cafa9d5
thelukass88 <l.holliday88@hotmail.com>
2024-01-15 13:14:15 +0000

    first question and options appear

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,13 +3,20 @@ const startButton = document.getElementById("start");
 const startPage = document.getElementById("start-screen");
 const questionNumber = questionsAndAnswers[0].number;
 const questionText = questionsAndAnswers[0].question;
+const optionOne = questionsAndAnswers[0].options[0];
+const optionTwo = questionsAndAnswers[0].options[1];
+const optionThree = questionsAndAnswers[0].options[2];
+const optionFour = questionsAndAnswers[0].options[3];
 
 
 function hideStart(_event){
     document.getElementById("start-screen").style.display="none";
     document.getElementById("questions").style.display="block";
     document.getElementById("question-title").innerHTML="Question " + questionNumber + questionText;
-
+    document.getElementById("choiceOne").innerHTML= optionOne;
+    document.getElementById("choiceTwo").innerHTML= optionTwo;
+    document.getElementById("choiceThree").innerHTML= optionThree;
+    document.getElementById("choiceFour").innerHTML= optionFour;
 }
 
 startButton.addEventListener("click", hideStart);

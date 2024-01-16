301e33653bab9156c9ee9de74bc99a5f636017fd
thelukass88 <l.holliday88@hotmail.com>
2024-01-16 18:54:50 +0000

    Added timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,18 +8,54 @@ const optionTwo = questionsAndAnswers[0].options[1];
 const optionThree = questionsAndAnswers[0].options[2];
 const optionFour = questionsAndAnswers[0].options[3];
 
+var timerElement = document.getElementById("time");
+var timer;
+var timerCount;
 
+//click away from the start page and into the quiz
 function hideStart(_event){
     document.getElementById("start-screen").style.display="none";
     document.getElementById("questions").style.display="block";
-    document.getElementById("question-title").innerHTML="Question " + questionNumber + questionText;
+    document.getElementById("question-title").innerHTML="Question " + questionNumber + ":" + questionText;
     document.getElementById("choiceOne").innerHTML= optionOne;
     document.getElementById("choiceTwo").innerHTML= optionTwo;
     document.getElementById("choiceThree").innerHTML= optionThree;
     document.getElementById("choiceFour").innerHTML= optionFour;
+    timerCount = 60;
+    startTimer()
 }
 
 startButton.addEventListener("click", hideStart);
 
+function questionEventListeners(){
+    optionOne.addEventListener('click', )
+
+}
+
+function checkAnswer(){
+    
+}
+
+
 //quiz timer
-let timer
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }

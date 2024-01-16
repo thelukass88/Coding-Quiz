0f6502f28bb0e1ce39bc449e8b0a7244dc24a09f
thelukass88 <l.holliday88@hotmail.com>
2024-01-16 22:46:31 +0000

    final code commit

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,15 @@
 
 const startButton = document.getElementById("start");
 const startPage = document.getElementById("start-screen");
-const questionNumber = questionsAndAnswers[0].number;
-const questionText = questionsAndAnswers[0].question;
+const questionButtons = document.getElementsByClassName("choiceButton");
+const questionNumberOne = questionsAndAnswers[0].number;
+const questionNumberTwo = questionsAndAnswers[1].number;
+const questionNumberThree = questionsAndAnswers[2].number;
+const questionNumberFour = questionsAndAnswers[3].number;
+const questionOneText = questionsAndAnswers[0].question;
+const questionTwoText = questionsAndAnswers[1].question;
+const questionThreeText = questionsAndAnswers[2].question;
+const questionFourText = questionsAndAnswers[3].question;
 const optionOne = questionsAndAnswers[0].options[0];
 const optionTwo = questionsAndAnswers[0].options[1];
 const optionThree = questionsAndAnswers[0].options[2];
@@ -12,11 +19,13 @@ var timerElement = document.getElementById("time");
 var timer;
 var timerCount;
 
+console.log(questionNumberTwo);
+
 //click away from the start page and into the quiz
 function hideStart(_event){
     document.getElementById("start-screen").style.display="none";
     document.getElementById("questions").style.display="block";
-    document.getElementById("question-title").innerHTML="Question " + questionNumber + ":" + questionText;
+    document.getElementById("question-title").innerHTML="Question " + questionNumberOne + ":" + questionOneText;
     document.getElementById("choiceOne").innerHTML= optionOne;
     document.getElementById("choiceTwo").innerHTML= optionTwo;
     document.getElementById("choiceThree").innerHTML= optionThree;
@@ -27,10 +36,18 @@ function hideStart(_event){
 
 startButton.addEventListener("click", hideStart);
 
-function questionEventListeners(){
-    optionOne.addEventListener('click', )
+function questionEventListeners(_event){
+    document.getElementById("question-title").innerHTML="Question " + questionNumberTwo + ":" + questionTwoText;
+    document.getElementById("choiceOne").innerHTML= optionOne;
+    document.getElementById("choiceTwo").innerHTML= optionTwo;
+    document.getElementById("choiceThree").innerHTML= optionThree;
+    document.getElementById("choiceFour").innerHTML= optionFour;
 
 }
+
+console.log(questionEventListeners);
+debugger
+questionButtons.addEventListener("click", questionEventListeners);
 
 function checkAnswer(){
     
@@ -45,11 +62,6 @@ function startTimer() {
       timerElement.textContent = timerCount;
       if (timerCount >= 0) {
         // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          winGame();
-        }
       }
       // Tests if time has run out
       if (timerCount === 0) {
